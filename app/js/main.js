d6f41e494b0400a106ee7bdbfcf76a0ae5252f7b
Nikita Elagin <34750690+Soveliy@users.noreply.github.com>
2025-07-29 17:03:48 +0300

    feat: add animation to hero

--- app/js/main.js
+++ app/js/main.js
@@ -30437,36 +30437,28 @@ gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.registerPlugin(gsap_MorphSVGPlugin_js__WE
 gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.registerPlugin(gsap_ScrollTrigger_js__WEBPACK_IMPORTED_MODULE_2__.ScrollTrigger);
 gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.set(".hero__title, .hero__desc, .hero__slide-wrap, .hero__button", {
   opacity: 0,
-  y: 60,
+  // y: 60,
   filter: "blur(10px)"
 });
 const headerTl = gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.timeline();
-setTimeout(() => {
-  headerTl.to(".hero__title, .hero__desc, .hero__slide-wrap, .hero__button", {
-    opacity: 1,
-    y: 0,
-    filter: "blur(0px)",
-    duration: 0.6,
-    ease: "expo.out",
-    stagger: 0.12
-  }, "-=0.5").to(".hero__button", {
-    scale: 1.05,
-    boxShadow: "0 8px 24px rgba(0,0,0,0.1)",
-    duration: 0.15,
-    yoyo: true,
-    repeat: 1,
-    ease: "power1.inOut"
-  }, "-=0.4").to(".header", {
-    y: 0,
-    opacity: 1,
-    duration: 0.7,
-    ease: "expo.out"
-  }).to(".hero__picture", {
-    opacity: 1,
-    duration: 0.7,
-    ease: "back.out(1.7)"
-  }, "-=0.3");
-}, 100);
+headerTl.to(".hero__title, .hero__desc, .hero__slide-wrap, .hero__button", {
+  opacity: 1,
+  y: 0,
+  filter: "blur(0px)",
+  duration: 0.6,
+  ease: "expo.out",
+  stagger: 0.12
+}).to(".hero__picture", {
+  opacity: 1,
+  duration: 0.7,
+  x: 0,
+  ease: "expo.out"
+}, "-=0.3").to(".header", {
+  y: 0,
+  opacity: 1,
+  duration: 0.7,
+  ease: "expo.out"
+}, "-=0.9");
 
 /***/ }),
 
--- src/js/components/animation.js
+++ src/js/components/animation.js
@@ -17,47 +17,34 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.set(".hero__title, .hero__desc, .hero__slide-wrap, .hero__button", {
   opacity: 0,
-  y: 60,
+  // y: 60,
   filter: "blur(10px)",
 });
 
 const headerTl = gsap.timeline();
 
-setTimeout(() => {
-  headerTl
-    .to(
-      ".hero__title, .hero__desc, .hero__slide-wrap, .hero__button",
-      {
-        opacity: 1,
-        y: 0,
-        filter: "blur(0px)",
-        duration: 0.6,
-        ease: "expo.out",
-        stagger: 0.12,
-      },
-      "-=0.5"
-    )
-    .to(
-      ".hero__button",
-      {
-        scale: 1.05,
-        boxShadow: "0 8px 24px rgba(0,0,0,0.1)",
-        duration: 0.15,
-        yoyo: true,
-        repeat: 1,
-        ease: "power1.inOut",
-      },
-      "-=0.4"
-    )
-    .to(".header", {
+headerTl
+  .to(".hero__title, .hero__desc, .hero__slide-wrap, .hero__button", {
+    opacity: 1,
+    y: 0,
+    filter: "blur(0px)",
+    duration: 0.6,
+    ease: "expo.out",
+    stagger: 0.12,
+  })
+
+  .to(
+    ".hero__picture",
+    { opacity: 1, duration: 0.7, x: 0, ease: "expo.out" },
+    "-=0.3"
+  )
+  .to(
+    ".header",
+    {
       y: 0,
       opacity: 1,
       duration: 0.7,
       ease: "expo.out",
-    })
-    .to(
-      ".hero__picture",
-      { opacity: 1, duration: 0.7, ease: "back.out(1.7)" },
-      "-=0.3"
-    );
-}, 100);
+    },
+    "-=0.9"
+  );
