45cfefbacf34dc08c35b5a6f1c371f44e3da8d9c
Nikita Elagin <34750690+Soveliy@users.noreply.github.com>
2025-07-30 14:51:51 +0300

    feat: add desktop animation for problems

--- app/js/main.js
+++ app/js/main.js
@@ -30445,20 +30445,72 @@ headerTl.to(".hero__title, .hero__desc, .hero__slide-wrap, .hero__button", {
   opacity: 1,
   y: 0,
   filter: "blur(0px)",
-  duration: 0.6,
-  ease: "expo.out",
+  duration: 1.2,
+  ease: "expo.inOut",
   stagger: 0.12
 }).to(".hero__picture", {
   opacity: 1,
-  duration: 0.7,
+  duration: 1,
   x: 0,
-  ease: "expo.out"
-}, "-=0.3").to(".header", {
+  ease: "expo.inOut"
+}, "-=0.8").to(".header", {
   y: 0,
   opacity: 1,
-  duration: 0.7,
-  ease: "expo.out"
+  duration: 1,
+  ease: "expo.inOut"
 }, "-=0.9");
+gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.registerPlugin(gsap_ScrollTrigger_js__WEBPACK_IMPORTED_MODULE_2__.ScrollTrigger);
+
+// gsap.to(".problems .fadeDown", {
+//   scrollTrigger: {
+//     trigger: ".problems",
+//     start: "top 50%", // когда верх блока на 80% от верха окна
+//     toggleActions: "play none none none",
+//   },
+//   opacity: 1,
+//   y: 0,
+//   filter: "blur(0px)",
+//   duration: 0.6,
+//   ease: "expo.out",
+//   stagger: 0.12,
+// });
+
+gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.utils.toArray(".fadeDown:not(.hero__title):not(.hero__desc):not(.hero__slide-wrap):not(.hero__button)").forEach((el, i) => {
+  gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.to(el, {
+    scrollTrigger: {
+      trigger: el,
+      start: "top 80%",
+      // когда верх элемента дойдет до 80% от окна
+      toggleActions: "play none none none"
+    },
+    opacity: 1,
+    y: 0,
+    filter: "blur(0px)",
+    duration: 1.2,
+    ease: "expo.inOut"
+    // delay: i * 0.12,
+  });
+});
+
+// Перебираем все .highlighted-text элементы
+document.querySelectorAll(".highlighted-text").forEach(highlight => {
+  const path = highlight.querySelector(".highlight-svg path");
+  if (!path) return; // если внутри нет path — пропускаем
+
+  const length = path.getTotalLength();
+  path.style.strokeDasharray = length;
+  path.style.strokeDashoffset = length;
+  gsap__WEBPACK_IMPORTED_MODULE_0__.gsap.to(path, {
+    strokeDashoffset: 0,
+    duration: 2.5,
+    ease: "expo.inOut",
+    scrollTrigger: {
+      trigger: highlight,
+      start: "top 90%",
+      toggleActions: "play none none none"
+    }
+  });
+});
 
 /***/ }),
 
--- src/js/components/animation.js
+++ src/js/components/animation.js
@@ -28,23 +28,81 @@ headerTl
     opacity: 1,
     y: 0,
     filter: "blur(0px)",
-    duration: 0.6,
-    ease: "expo.out",
+    duration: 1.2,
+    ease: "expo.inOut",
     stagger: 0.12,
   })
 
   .to(
     ".hero__picture",
-    { opacity: 1, duration: 0.7, x: 0, ease: "expo.out" },
-    "-=0.3"
+    { opacity: 1, duration: 1, x: 0, ease: "expo.inOut" },
+    "-=0.8"
   )
   .to(
     ".header",
     {
       y: 0,
       opacity: 1,
-      duration: 0.7,
-      ease: "expo.out",
+      duration: 1,
+      ease: "expo.inOut",
     },
     "-=0.9"
   );
+
+gsap.registerPlugin(ScrollTrigger);
+
+// gsap.to(".problems .fadeDown", {
+//   scrollTrigger: {
+//     trigger: ".problems",
+//     start: "top 50%", // когда верх блока на 80% от верха окна
+//     toggleActions: "play none none none",
+//   },
+//   opacity: 1,
+//   y: 0,
+//   filter: "blur(0px)",
+//   duration: 0.6,
+//   ease: "expo.out",
+//   stagger: 0.12,
+// });
+
+gsap.utils
+  .toArray(
+    ".fadeDown:not(.hero__title):not(.hero__desc):not(.hero__slide-wrap):not(.hero__button)"
+  )
+  .forEach((el, i) => {
+    gsap.to(el, {
+      scrollTrigger: {
+        trigger: el,
+        start: "top 80%", // когда верх элемента дойдет до 80% от окна
+        toggleActions: "play none none none",
+      },
+      opacity: 1,
+      y: 0,
+      filter: "blur(0px)",
+      duration: 1.2,
+      ease: "expo.inOut",
+      // delay: i * 0.12,
+    });
+  });
+
+// Перебираем все .highlighted-text элементы
+document.querySelectorAll(".highlighted-text").forEach((highlight) => {
+  const path = highlight.querySelector(".highlight-svg path");
+  if (!path) return; // если внутри нет path — пропускаем
+
+  const length = path.getTotalLength();
+
+  path.style.strokeDasharray = length;
+  path.style.strokeDashoffset = length;
+
+  gsap.to(path, {
+    strokeDashoffset: 0,
+    duration: 2.5,
+    ease: "expo.inOut",
+    scrollTrigger: {
+      trigger: highlight,
+      start: "top 90%",
+      toggleActions: "play none none none",
+    },
+  });
+});
